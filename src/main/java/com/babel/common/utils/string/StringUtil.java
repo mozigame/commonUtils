690c7df72969ab87a7c2b5ea934e987dd1c53ded
roach <roach@qq.com>
2017-09-27 14:33:51 +0800

    up

---
+++ src/main/java/com/babel/common/utils/string/StringUtil.java
@@ -0,0 +1,25 @@
+package com.babel.common.utils.string;
+
+/**
+ * created by roachjiang 2017/9/27
+ */
+public class StringUtil {
+
+
+    public static final String NO_DATA_HINT = "-";
+
+
+    public static String joinMsg(Object ...msg){
+        if (msg==null){
+            return "";
+        }
+        StringBuilder stringBuilder = new StringBuilder();
+        for (Object ele:msg){
+            if (ele!=null){
+                stringBuilder.append(ele);
+            }
+        }
+        return stringBuilder.toString();
+    }
+
+}
