b164f3ad83ce73bea3a7e7f49496be7bfd422791
justin <justins1707@gmail.com>
2018-01-05 16:36:49 +0800

    update

--- src/main/java/com/babel/common/lottery/constant/LotteryNoPayoff.java
+++ src/main/java/com/babel/common/lottery/constant/LotteryNoPayoff.java
@@ -1,5 +1,6 @@
 package com.babel.common.lottery.constant;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
@@ -8,4 +9,19 @@ import java.util.List;
  */
 public class LotteryNoPayoff {
     public static List<Long> PLAYS_SIX = Arrays.asList(1151190l, 1111190l, 1111290l, 1111390l, 1111490l, 1141190l,1141290l,1141390l,1141490l);
+    public static List<Long> PLAYS_SSC = Arrays.asList(113l,213l,323l,324l,423l,424l,523l,524l);
+
+    public static List<Long> getPlays(Long lotteryId){
+        List<Long> list=new ArrayList<>();
+        if(lotteryId==null){
+            return list;
+        }
+        if(lotteryId==10){
+            return PLAYS_SIX;
+        }
+        else if(lotteryId==1||lotteryId==2){
+            return PLAYS_SSC;
+        }
+        return list;
+    }
 }
