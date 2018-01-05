d33a429577553a130882e4553542f0a0f272697b
justin <justins1707@gmail.com>
2018-01-05 16:29:26 +0800

    update

---
+++ src/main/java/com/babel/common/lottery/constant/LotteryNoPayoff.java
@@ -0,0 +1,11 @@
+package com.babel.common.lottery.constant;
+
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * Created by Owner on 2018/1/5.
+ */
+public class LotteryNoPayoff {
+    public static List<Long> PLAYS_SIX = Arrays.asList(1151190l, 1111190l, 1111290l, 1111390l, 1111490l, 1141190l,1141290l,1141390l,1141490l);
+}
